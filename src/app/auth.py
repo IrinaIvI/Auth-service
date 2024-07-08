584926e61ac82395338f23df3b6a06ecdb7c2ff9
Irina Ivashko <ira.ivashko.99@gmail.com>
2024-07-08 10:10:47 +0000

    Исправлены некоторые функции

--- src/app/auth.py
+++ src/app/auth.py
@@ -10,20 +10,20 @@ users = {}
 class Authentication:
     """Класс аутентификации пользователя."""
 
-    def registration(self, login: str, password: str) -> dict:
+    def registration(self, login: str, password: str) -> str:
         """Регистрация пользователя."""
         payload = {'user_login': login, 'password': password}
         token = jwt.encode(payload, SECRET_KEY, ALGORITHM)
         users[login] = token
-        return {'token': token}
+        return token
 
-    def authorisation(self, login: str, password: str) -> dict:
+    def authorisation(self, login: str, password: str) -> str:
         """Авторизация пользователя."""
         try:
             if login in users:
                 payload = {'user_login': login, 'password': password}
                 token = jwt.encode(payload, SECRET_KEY, ALGORITHM)
                 if users.get(login, token) == token:
-                    return {'token': token}
+                    return token
         except jwt.PyJWTError:
             return None
