898d8c472f0cf3bd9b883b4510149dbb12914d0c
Irina Ivashko <ira.ivashko.99@gmail.com>
2024-07-08 10:09:51 +0000

    Добавлены новые тесты

--- src/tests/test_auth.py
+++ src/tests/test_auth.py
@@ -1,2 +1,14 @@
-from app.auth import Authentication
+from app.auth import Authentication, users, SECRET_KEY, ALGORITHM
 import pytest
+import jwt
+
+
+@pytest.mark.parametrize('login, password', [
+    pytest.param('mike', 'superboss', id='is correct')
+])
+def test_registration(login, password):
+    token = jwt.encode({'user_login': login, 'password': password}, SECRET_KEY, ALGORITHM)
+    assert Authentication().registration(login, password) == token
+
+def test_authorisation():
+    pass
