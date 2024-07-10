40755a4a0a7040bc22497328f43359ed4a1a8d2d
Irina Ivashko <ira.ivashko.99@gmail.com>
2024-07-10 07:56:01 +0000

    Изменение тестов

--- src/tests/test_auth.py
+++ src/tests/test_auth.py
@@ -1,4 +1,4 @@
-from app.auth import Authentication, users, SECRET_KEY, ALGORITHM
+from app.auth import Authentication, SECRET_KEY, ALGORITHM, users
 import pytest
 import jwt
 
@@ -7,8 +7,21 @@ import jwt
     pytest.param('mike', 'superboss', id='is correct')
 ])
 def test_registration(login, password):
+    Authentication().registration(login, password)
     token = jwt.encode({'user_login': login, 'password': password}, SECRET_KEY, ALGORITHM)
-    assert Authentication().registration(login, password) == token
+    assert users[login] == token
 
-def test_authorisation():
-    pass
+@pytest.mark.parametrize('login, password', [
+    pytest.param('mike', 'superboss', id='is correct'),
+    pytest.param('', 'superboss', id='is not correct'),
+    pytest.param('mike', '', id='is not correct'),
+    pytest.param('mike', 'superboss', id='is correct'),
+    pytest.param('', '', id='is correct'),
+])
+def test_authorisation(login, password):
+    Authentication().registration(login, password)
+    token = jwt.encode({'user_login': login, 'password': password}, SECRET_KEY, ALGORITHM)
+    assert Authentication().authorisation(login, password) == token
+
+
+
