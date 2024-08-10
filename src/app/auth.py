f84236591a7d6a8e7e218698e2bba9719b1d75c2
Irina Ivashko <ira.ivashko.99@gmail.com>
2024-08-10 21:18:28 +0300

    Написание readme, contributing и changelog

--- src/app/auth.py
+++ src/app/auth.py
@@ -25,25 +25,35 @@ class Authentication:
 
     def registration(self, login: str, password: str) -> User:
         """Регистрация пользователя."""
-        payload = {'user_login': login, 'password': password}
-        token = jwt.encode(payload, SECRET_KEY, ALGORITHM)
-        hashed_password = hash(password)
-        user = User(login, hashed_password, token)
-        users.append(user)
-        return user
+        if not login.strip() or not password.strip():
+            return 'Неправильно введены данные'
+        else:
+            payload = {'user_login': login, 'password': password}
+            token = jwt.encode(payload, SECRET_KEY, ALGORITHM)
+            hashed_password = hash(password)
+            user = User(login, hashed_password, token)
+            users.append(user)
+            return user
+
 
     def authorisation(self, login: str, password: str) -> Union[str, bool]:
         """Авторизация пользователя."""
-        try:
-            for user in users:
-                if user.login == login and user.hashed_password == hash(password):
-                    return user.token
-        except jwt.PyJWTError:
-            return None
+        if not login.strip() or not password.strip():
+            return 'Неправильно введены данные'
+        else:
+            try:
+                for user in users:
+                    if user.login == login and user.hashed_password == hash(password):
+                        return user.token
+            except jwt.PyJWTError:
+                return None
 
-    def validate(self, token: str) -> bool:
+
+    def validate(self, id: int, token: str) -> bool:
         """Проверка токена на валидность."""
         for user in users:
-            if user.token == token:
-                raise HTTPException(status_code=200, detail='OK')
-        raise HTTPException(status_code=401, detail='Unauthorised')
+            if user.id == id:
+                if user.token == token:
+                    raise HTTPException(status_code=200, detail='OK')
+            raise HTTPException(status_code=401, detail='Unauthorised')
+
