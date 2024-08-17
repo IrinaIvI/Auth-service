d6444f6731c86ddccaba9c63927cd0e9f707df1a
Irina Ivashko <ira.ivashko.99@gmail.com>
2024-08-17 20:38:17 +0300

    Проведена миграция таблиц

--- src/app/models.py
+++ src/app/models.py
@@ -1,12 +1,10 @@
 from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, ForeignKey
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = "users"
-    __table_args__ = {'schema': 'auth_schema'}
+    __tablename__ = "users_ivashko"
 
     id = Column(Integer, primary_key=True)
     login = Column(String, unique=True, nullable=False)
@@ -15,18 +13,13 @@ class User(Base):
     created_at = Column(TIMESTAMP, nullable=True, default=None)
     updated_at = Column(TIMESTAMP, nullable=True, default=None)
 
-    user_tokens = relationship("UserToken", back_populates="user")
-
 
 class UserToken(Base):
-    __tablename__ = "usertoken"
-    __table_args__ = {'schema': 'auth_schema'}
+    __tablename__ = "usertoken_ivashko"
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('auth_schema.users.id'))
+    user_id = Column(Integer, ForeignKey('users_ivashko.id'))
     token = Column(String, unique=True, nullable=False)
     is_valid = Column(Boolean, default=False)
     expiration_at = Column(TIMESTAMP, nullable=True, default=None)
     updated_at = Column(TIMESTAMP, nullable=True, default=None)
-
-    user = relationship("User", back_populates="user_tokens")
